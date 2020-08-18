5d453a1d04538e8a3240549fd5442fdee459eac8
Chris Owens <guitarrockstarco@gmail.com>
2020-08-18 16:03:41 -0400

    Added a buttom

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ const verb1 = document.getElementById('verb1');
 const noun1Input = document.getElementById('noun1Input');
 const noun2Input = document.getElementById('noun2Input');
 const verb1Input = document.getElementById('verb1Input')
+const bodyContainer = document.getElementById('container');
+const radButton = document.getElementById('radButton');
+const additionalText = "Hey, this is rad!!!";
 
 myButton.addEventListener('click', function (event) {
   event.preventDefault();
@@ -12,4 +15,13 @@ myButton.addEventListener('click', function (event) {
   noun1.innerHTML = noun1Input.value;
   noun2.innerHTML = noun2Input.value;
   verb1.innerHTML = verb1Input.value
+});
+
+radButton.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const newParagraph = document.createElement("p");
+  newParagraph.innerHTML = additionalText;
+  bodyContainer.appendChild(newParagraph);
+
 });
